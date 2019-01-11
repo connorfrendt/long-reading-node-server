32f95d3e4c92df7ea7f06e857fbfe3009bebcfb8
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-10 22:58:43 -0800

    putting in non blocking operators

--- long-reading/requestHandlers.js
+++ long-reading/requestHandlers.js
@@ -1,11 +1,24 @@
-function start() {
+var exec = require('child_process').exec;
+
+function start(response) {
+  /* eslint-disable-next-line */
   console.log('Request handler "start" was called.');
-  return 'Hello Start';
+
+  exec('find /',
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function(err, stdout, stderr) {
+      response.writeHead(22, { 'Content-Type': 'text/plain' });
+      response.write(stdout);
+      response.end();    
+    });
 }
 
-function upload() {
+function upload(response) {
+  /* eslint-disable-next-line */
   console.log('Request handler "upload" was called.');
-  return 'Hello Upload';
+  response.writeHead(22, { 'Content-Type': 'text/plain' });
+  response.write('Hello Upload');
+  response.end();
 }
 
 exports.start = start;
--- long-reading/server.js
+++ long-reading/server.js
@@ -4,16 +4,14 @@ var url = require('url');
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
+    /* eslint-disable-next-line */
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname);
-
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
+    route(handle, pathname, response);
   }
   
   http.createServer(onRequest).listen(8888);
+  /* eslint-disable-next-line */
   console.log('Server has started.');
 }
 
